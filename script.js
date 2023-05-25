9892b140cec8cd40e7741e15112be19451c295d7
Felipe Duarte <feduarte.dev@gmail.com>
2023-05-25 09:03:44 -0300

    quinto requisito completo

--- script.js
+++ script.js
@@ -3,12 +3,16 @@ const cartaGerada = document.getElementById('carta-gerada');
 const cardBtn = document.getElementById('criar-carta');
 
 cardBtn.addEventListener('click', () => {
-  let card = inputText.value;
-  let cardArray = card.split(' ');
-  cartaGerada.innerText = '';
-  for (let index = 0; index < cardArray.length; index += 1) {
-    const span = document.createElement('span');
-    span.innerText = cardArray[index];
-    cartaGerada.appendChild(span);
+  if (inputText.value === '' || inputText.value === ' ') {
+    cartaGerada.innerText = 'Por favor, digite o conteúdo da carta.';
+  } else {
+    let card = inputText.value;
+    let cardArray = card.split(' ');
+    cartaGerada.innerText = '';
+    for (let index = 0; index < cardArray.length; index += 1) {
+      const span = document.createElement('span');
+      span.innerText = cardArray[index];
+      cartaGerada.appendChild(span);
+    }
   }
 });
