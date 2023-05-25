47693ac8af0ad34727f1cd3a9c09f4fae8f5ec9d
Felipe Duarte <feduarte.dev@gmail.com>
2023-05-25 11:30:51 -0300

    18 requisito completo

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const inputText = document.getElementById('carta-texto');
 const cartaGerada = document.getElementById('carta-gerada');
 const cardBtn = document.getElementById('criar-carta');
+const contador = document.getElementById('carta-contador');
+const contapalavra = document.getElementsByTagName('span');
+
+const randomThree = () => {
+  return Math.floor(Math.random() * 3);
+};
+
+const randomTwo = () => {
+  return Math.floor(Math.random() * 2);
+};
+
+const count = () => {
+  contador.innerText = contapalavra.length;
+};
+
 
 cardBtn.addEventListener('click', () => {
   if (inputText.value === '' || inputText.value === ' ') {
@@ -11,8 +26,75 @@ cardBtn.addEventListener('click', () => {
     cartaGerada.innerText = '';
     for (let index = 0; index < cardArray.length; index += 1) {
       const span = document.createElement('span');
+      
+      if (randomThree() === 0) {
+        span.classList.add('newspaper');
+      } else if (randomThree() === 1) {
+        span.classList.add('magazine1');
+      } else {
+        span.classList.add('magazine2');
+      }
+
+      if (randomThree() === 0) {
+        span.classList.add('medium');
+      } else if (randomThree() === 1) {
+        span.classList.add('big');
+      } else {
+        span.classList.add('reallybig');
+      }
+
+      if (randomTwo() === 0) {
+        span.classList.add('rotateleft');
+      } else {
+        span.classList.add('rotateright');
+      }
+
+      if (randomTwo() === 0) {
+        span.classList.add('skewleft');
+      } else {
+        span.classList.add('skewright');
+      }
+
       span.innerText = cardArray[index];
       cartaGerada.appendChild(span);
     }
-  }
+  } count();
 });
+
+
+// const styleGroup = () => {
+//   if (randomThree() === 0) {
+//     span.classList.add('newspaper');
+//   } else if (randomThree() === 1) {
+//     span.classList.add('magazine1');
+//   } else {
+//     span.classList.add('magazine2');
+//   }
+// };
+
+// const sizeGroup = () => {
+//   if (randomThree() === 0) {
+//     span.classList.add('medium');
+//   } else if (randomThree() === 1) {
+//     span.classList.add('big');
+//   } else {
+//     span.classList.add('reallybig');
+//   }
+// };
+
+// const rotateGroup = () => {
+//   if (randomTwo() === 0) {
+//     span.classList.add('rotateleft');
+//   } else {
+//     span.classList.add('rotateright');
+//   }
+// };
+
+// const skewGroup = () => {
+//   if (randomTwo() === 0) {
+//     span.classList.add('skewleft');
+//   } else {
+//     span.classList.add('skewright');
+//   }
+// };
+
